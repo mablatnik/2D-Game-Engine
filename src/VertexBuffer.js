44334e95b52d8fc3fe7ed91d1756143cdcf0d6ee
Mark Blatnik <mablatnik@gmail.com>
2016-10-27 16:22:09 +0300

    Add GLSL shader and fragment shader

--- src/VertexBuffer.js
+++ src/VertexBuffer.js
@@ -13,11 +13,11 @@ function initSquareBuffer() {
         0.5, 0.5, 0.0,
         -0.5, 0.5, 0.0,
         0.5, -0.5, 0.0,
-        -0.5, -0.5, 0.0,
+        -0.5, -0.5, 0.0
     ];
     
-    gSquareVertexBuffer = gGL.createBugger();
+    gSquareVertexBuffer = gGL.createBuffer();
     gGL.bindBuffer(gGL.ARRAY_BUFFER, gSquareVertexBuffer);
-    gGL.bufferData(gGL.ARRAY_BUFFER, new Fload32Array(verticesOfSquare),
+    gGL.bufferData(gGL.ARRAY_BUFFER, new Float32Array(verticesOfSquare), 
     gGL.STATIC_DRAW);
 }
