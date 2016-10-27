9f29eb943935fee9a196697469a17c584b8960b4
Mark Blatnik <mablatnik@gmail.com>
2016-10-27 09:27:44 +0300

    feat: Move JavaScript to js file
    The reason behind this is that as the code gets more complex
    the the clustering of functionality can become unmanageable.

--- src/WebGL.js
+++ src/WebGL.js
@@ -3,5 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+"use strict";
 
+var gGL = null;
 
+function initializeGL() {
+    var canvas = document.getElementById("GLCanvas");
+            var gl = canvas.getContext("webgl") || 
+                    canvas.getContext("experimental-webgl");
+            if(gl !== null){
+                gl.clearColor(0.0, 0.8, 0.0, 1.0);
+            } else {
+                document.write("<br><b>WebGL is not supported!<b>");
+            }
+}
