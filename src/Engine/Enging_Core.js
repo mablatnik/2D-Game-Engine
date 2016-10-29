fe2883e1a10c4a4c9a2e95bf516611a0b6372b28
Mark Blatnik <mablatnik@gmail.com>
2016-10-29 12:29:38 +0300

    Create the core of the game engine

--- src/Engine/Enging_Core.js
+++ src/Engine/Enging_Core.js
@@ -1,1 +1,17 @@
 
+"use strict";
+
+// initialize the variable and ensure it is not redefined
+var gEngine = gEngine || {};
+
+gEngine.Core = (function() {
+    // the graphic context for drawing
+    var mGL = null;
+    // accessor of the webgl context
+    var getGL = function() {return mGL;};
+    // contains functions and variables that will be accessible
+    var mPublic = {
+        getGL: getGL
+    };
+    return mPublic;
+}());
