bae0952345fa21370ba9faa83112609a0af11475
Mark Blatnik <mablatnik@gmail.com>
2016-10-30 09:21:34 +0300

    Create function to initialize WebGL, buffer, and compile shaders

--- src/Engine/Enging_Core.js
+++ src/Engine/Enging_Core.js
@@ -9,6 +9,18 @@ gEngine.Core = (function() {
     var mGL = null;
     // accessor of the webgl context
     var getGL = function() {return mGL;};
+    // initialize the WebGL, buffer, and compile shaders
+    var initializeWebGL = function(htmlCanvasID) {
+        var canvas = document.getElementById(htmlCanvasID);
+        // get WebGL and bind to canvas
+        mGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+        if(mGL == null) {
+            document.write("<br><b>WebGL is not supported!</b>");
+            return;
+        }
+        // initialize the VertexBuffer
+        gEnging.VertexBuffer.initialize();
+    };
     // contains functions and variables that will be accessible
     var mPublic = {
         getGL: getGL
