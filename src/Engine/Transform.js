597656d20a0a7d28c89977230aaf49bf44608998
Mark Blatnik <mablatnik@gmail.com>
2016-11-01 15:49:06 +0300

    Create the transform object

---
+++ src/Engine/Transform.js
@@ -0,0 +1,48 @@
+function Transform() {
+    // translation operator
+    this.mPosition = vec2.fromValues(0,0);
+    // scaling operator
+    this.mScale = vec2.fromValues(1,1);
+    // rotation in radians
+    this.mRotationInRad = 0.0;
+};
+
+// position getters and setters
+Transform.prototype.setPosition = function (xPos, yPos) {
+    this.setXPos(xPos);
+    this.setYPos(yPos);
+};
+
+Transform.prototype.getPosition = function () {return this.mPosition;};
+
+// size setters and getters
+Transform.prototype.setSize = function (width, height) {
+    this.setWidth(width);
+    this.setHeight(height);
+};
+
+Transform.prototype.getSize = function() {return this.mScale;};
+
+// rotation getters and setters
+Transform.prototype.setRotationInRad = function (rotationInRadians) {
+    this.mRotationInRad = rotationInRadians;
+    while(this.mRotationInRad > (2*Math.PI))
+        this.mRotationInRad -= (2*Math.PI);
+};
+
+Transform.prototype.setRotationInDegree = function (rotationInDegree) {
+    this.setRotationInRad(rotationInDegree*Math.PI/180.0);
+};
+
+Transform.prototype.getXform = function() {
+    // create blank identity matrix
+    var matrix = mat4.create();
+    // compute translation: z is zero for 2D game
+    mat4.translate(matrix, matrix, vec3.fromValues(this.getXPos(), this.getYPos(), 0.0));
+    // concatenate with rotation
+    mat4.rotateZ(matrix, matrix, this.getRotationInRad());
+    // concatenate with scaling
+    mat4.scale(matrix, matrix, vec3.fromValues(this.getWidth(), this.getHeight(), 1.0));
+    return matrix;
+};
+
