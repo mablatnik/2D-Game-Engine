1dc766bf556993c7b96208d1470843206f8e727a
Mark Blatnik <mablatnik@gmail.com>
2016-11-04 10:22:01 +0300

    Add function to run game loop

--- src/Engine/Core/Engine_GameLoop.js
+++ src/Engine/Core/Engine_GameLoop.js
@@ -1,7 +1,45 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var gEngine = gEngine || {};
 
-
+gEngine.GameLoop = (function() {
+    // frames per second
+    var kFPS = 60;
+    // Milleseconds per frame
+    var kMPF = 1000 / kFPS;
+    
+    // variables for timing gameloop
+    var mPreviousTime;
+    var mLagTime;
+    var CurrentTime;
+    var mElapsedTime;
+    
+    // current loop state
+    var mIsLoopRunning = false;
+    
+    // reference to game logic
+    var mMyGame = null;
+    
+    // sub-classed from MyGame
+    var _runLoop = function () {
+        if(mIsLoopRunning) {
+            // set up for next call and update
+            requestAnimationFrame(function(){_runLoop.call(mMyGame);});
+            // compute the elapsed time since last loop run
+            mCurrentTime = Date.now();
+            mElapsedTime = mCurrentTime - mPreviousTime;
+            mPreviousTime = mCurrentTime;
+            mLagTime += mElapsedTime;
+            // update game appropriate number of times
+            while((mLagTime >= kMPF) && mIsLoopRunning) {
+                // call MyGame.update()
+                this.update();
+                mLagTime -= kMPF;
+            }
+            this.draw();
+        }
+    };
+    
+    var mPublic = {
+        
+    };
+    return mPublic;
+}());
