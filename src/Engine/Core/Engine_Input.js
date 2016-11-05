89fcd4ae76137e2c4fe1c88e0df171aceada53ba
Mark Blatnik <mablatnik@gmail.com>
2016-11-05 11:41:35 +0300

    Add key codes

--- src/Engine/Core/Engine_Input.js
+++ src/Engine/Core/Engine_Input.js
@@ -1,7 +1,49 @@
 var gEngine = gEngine || {};
 
 gEngine.Input = (funtion() {
-    var mPublic = {};
+    // key codes
+    var kKeys = {
+        // arrows
+        Left: 37,
+        Up: 38,
+        Right: 39,
+        Down: 40,
+
+        // space bar
+        Space: 32,
+
+        // numbers 
+        Zero: 48,
+        One: 49,
+        Two: 50,
+        Three: 51,
+        Four: 52,
+        Five : 53,
+        Six : 54,
+        Seven : 55,
+        Eight : 56,
+        Nine : 57,
+
+        // Alphabets
+        A : 65,
+        D : 68,
+        E : 69,
+        F : 70,
+        G : 71,
+        I : 73,
+        J : 74,
+        K : 75,
+        L : 76,
+        R : 82,
+        S : 83,
+        W : 87,
+
+        LastKeyCode: 222
+    };
+    
+    var mPublic = {
+        
+    };
     return mPublic;
 }());
 
