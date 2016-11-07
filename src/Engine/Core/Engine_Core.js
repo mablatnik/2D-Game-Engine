eab027ca32060bc3ccdea3e4f3de0492e51abf25
Mark Blatnik <mablatnik@gmail.com>
2016-11-07 08:11:32 +0300

    Modify engine to support keyboard input

--- src/Engine/Core/Engine_Core.js
+++ src/Engine/Core/Engine_Core.js
@@ -6,10 +6,9 @@ var gEngine = gEngine || {};
 gEngine.Core = (function() {
     // the graphic context for drawing
     var mGL = null;
-    // accessor of the webgl context
-    var getGL = function() {return mGL;};
+    
     // initialize the WebGL, buffer, and compile shaders
-    var initializeWebGL = function(htmlCanvasID) {
+    var _initializeWebGL = function(htmlCanvasID) {
         var canvas = document.getElementById(htmlCanvasID);
         // get WebGL and bind to canvas
         mGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
@@ -17,9 +16,18 @@ gEngine.Core = (function() {
             document.write("<br><b>WebGL is not supported!</b>");
             return;
         }
-        // initialize the VertexBuffer
-        gEngine.VertexBuffer.initialize();
     };
+    
+    // accessor of the webgl context
+    var getGL = function() {return mGL;};
+    
+    // initialize all of the EngineCore components
+    var initializeEngineCore = function(htmlCanvasID) {
+        _initializeWebGL(htmlCanvasID);
+        gEngine.VertexBuffer.initialize();
+        gEngine.Input.initialize();
+    };
+    
     // clears the draw area and draws one square
     var clearCanvas = function(color) {
         mGL.clearColor(color[0], color[1], color[2], color[3]);
@@ -28,7 +36,7 @@ gEngine.Core = (function() {
     // contains functions and variables that will be accessible
     var mPublic = {
         getGL: getGL,
-        initializeWebGL: initializeWebGL,
+        initializeEngineCore: initializeEngineCore,
         clearCanvas: clearCanvas
     };
     return mPublic;
