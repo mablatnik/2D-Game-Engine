96b3670bda7ccf6c4b3c97bee08f694fc90b071a
Mark Blatnik <mablatnik@gmail.com>
2016-11-15 14:40:43 +0300

    feat: Create client interface to the game engine
    The goal of this feature is to create a scene object that acts as
    a superclass for interfacing with the game engine. All client code
    will become a subclass of this superclass.

---
+++ src/Engine/Scene.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function Scene() {}
+
+Scene.prototype.initialize = function() {
+    
+};
+
+Scene.prototype.loadScene = function() {
+    
+};
+
+Scene.prototype.unloadScene = function() {
+    
+};
+
+Scene.prototype.update = function() {
+    
+};
+
+Scene.prototype.draw = function() {
+
+};
