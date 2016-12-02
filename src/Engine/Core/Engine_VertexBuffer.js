5454a6da0129aae452b874210788fc050bf82cf8
Mark Blatnik <mablatnik@gmail.com>
2016-12-02 16:01:52 +0300

    Define and set up texture coordinates

--- src/Engine/Core/Engine_VertexBuffer.js
+++ src/Engine/Core/Engine_VertexBuffer.js
@@ -4,6 +4,12 @@ var gEngine = gEngine || {};
 
 // the VertexBuffer object
 gEngine.VertexBuffer = (function() {
+    // reference to the vertex positions for the square
+    var mSquareVertexBuffer = null;
+    
+    // reference to the texture positions for the square verticiew in gl position
+    var mTextureCoordBuffer = null;
+    
     // verticies of the square
     var verticesOfSquare = [
         0.5, 0.5, 0.0,
@@ -11,9 +17,15 @@ gEngine.VertexBuffer = (function() {
         0.5, -0.5, 0.0,
         -0.5, -0.5, 0.0
     ];
-    // reference to the vertex positions for the square
-    var mSquareVertexBuffer = null;
-    var getGLVertexRef = function() {return mSquareVertexBuffer;};
+    
+    // texture coordinates
+    var textureCoordinates = [
+        1.0, 1.0,
+        0.0, 1.0,
+        1.0, 0.0,
+        0.0, 0.0
+    ];
+    
     var initialize = function() {
         var gl = gEngine.Core.getGL();
         // create buffer
@@ -22,10 +34,24 @@ gEngine.VertexBuffer = (function() {
         gl.bindBuffer(gl.ARRAY_BUFFER, mSquareVertexBuffer);
         // load verticies into the vertexBuffer
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticesOfSquare), gl.STATIC_DRAW);
+        
+        // create buffer on gGL context
+        mTextureCoordBuffer = gl.createBuffer();
+        
+        // activate vertexBuffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, mTextureCoordBuffer);
+        
+        // load texture coordinates into the vertexBuffer
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
     };
+    
+    var getGLVertexRef = function() {return mSquareVertexBuffer; };
+    var getGLTexCoordRef = function() {return mTextureCoordBuffer; };
+    
     var mPublic = {
         initialize: initialize,
-        getGLVertexRef: getGLVertexRef
+        getGLVertexRef: getGLVertexRef,
+        getGLTextCoordRef: getGLTexCoordRef
     };
     return mPublic;
 }());
