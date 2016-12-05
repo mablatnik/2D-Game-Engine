4b48b02d9c927abbfc3e7a59f404c5e135e55634
Mark Blatnik <mablatnik@gmail.com>
2016-12-05 17:48:31 +0300

    Modify engine default ressources to support texture shaders

--- src/Engine/Core/Resources/Engine_DefaultResources.js
+++ src/Engine/Core/Resources/Engine_DefaultResources.js
@@ -3,14 +3,20 @@
 var gEngine = gEngine || { };
 
 gEngine.DefaultResources = (function () {
+    // simple shader
     // path to vertex shader
     var kSimpleVS = "src/GLSLShaders/SimpleVS.glsl";
     // Path to the simple FragmentShader
     var kSimpleFS = "src/GLSLShaders/SimpleFS.glsl";
     // variable for SimpleShader object
     var mConstColorShader = null;
-    // assessor
-    var getConstColorShader = function () { return mConstColorShader; };
+    
+    // texture shader
+    // path to vertex shader
+    var kTextureVS = "src/GLSLShaders/TextureVS.glsl";
+    // path to fragment shader
+    var kTextureFS = "src/GLSLShaders/TextureFS.glsl";
+    var mTextureShader = null;
     
     // callback function after loadings are done
     var _createShaders = function (callBackFunction) {
@@ -18,18 +24,27 @@ gEngine.DefaultResources = (function () {
         callBackFunction();
     };
     
+    // assessor
+    var getConstColorShader = function () { return mConstColorShader; };
+    var getTextureShader = function () { return mTextureShader; };
+    
     // initiate asynchronous loading of GLSL Shader files
     var initialize = function (callBackFunction) {
         // constant color shader: SimpleVS, and SimpleFS
         gEngine.TextFileLoader.loadTextFile(kSimpleVS, gEngine.TextFileLoader.eTextFileType.eTextFile);
         gEngine.TextFileLoader.loadTextFile(kSimpleFS, gEngine.TextFileLoader.eTextFileType.eTextFile);
+        
+        // texture shaders
+        gEngine.TextFileLoader.loadTextFile(kTextureVS, gEngine.TextFileLoader.eTextFileType.eTextFile);
+        gEngine.TextFileLoader.loadTextFile(kTextureFS, gEngine.TextFileLoader.eTextFileType.eTextFile);
 
         gEngine.ResourceMap.setLoadCompleteCallback(function () { _createShaders(callBackFunction); });
     };
     
     var mPublic = {
         initialize: initialize,
-        getConstColorShader: getConstColorShader
+        getConstColorShader: getConstColorShader,
+        getTextureShader: getTextureShader
     };
     return mPublic;
 }());
